28b1b608ebbbebc24216aba4ac398ca1be9b69b6
aneznamova <an.neznamova@gmail.com>
2016-02-25 19:12:52 +0300

    Добавлен класс Point, создана функция distance, результаты вычисления выводятся на экран и контролируются визуально

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,22 +1,23 @@
 package ru.stqa.ptf.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-    hello ("world");
-    hello ("user");
-    hello ("Anastasia");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(4, 7);
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Точка p1 = (" + p1.x + " , " + p1.y + ")");
+    System.out.println("Точка p2 = (" + p2.x + " , " + p2.y + ")");
+    System.out.println("Расстояние между точками р1 и р2 =" + distance(p1, p2));
+  }
 
-    	}
-
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
+    public static double distance(Point p1, Point p2) {
+    double x = p1.x - p2.x;
+    double y = p1.y - p2.y;
+    return Math.sqrt(x * x + y * y);
   }
 
 
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by neznaa on 2/25/2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+}
