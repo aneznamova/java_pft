bd78f3c523638a427d3cd92aba1f37e6d28bd071
aneznamova <an.neznamova@gmail.com>
2016-02-25 19:33:20 +0300

    Реализация задания при помощи метода в классе Point, добавлен пример использования метода вместо ранее созданной функции.

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -6,19 +6,13 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
+    Point p = new Point();
     Point p1 = new Point(1, 2);
     Point p2 = new Point(4, 7);
 
     System.out.println("Точка p1 = (" + p1.x + " , " + p1.y + ")");
     System.out.println("Точка p2 = (" + p2.x + " , " + p2.y + ")");
-    System.out.println("Расстояние между точками р1 и р2 =" + distance(p1, p2));
+    System.out.println("Расстояние между точками р1 и р2 =" + p.distance(p1, p2));
   }
-
-    public static double distance(Point p1, Point p2) {
-    double x = p1.x - p2.x;
-    double y = p1.y - p2.y;
-    return Math.sqrt(x * x + y * y);
-  }
-
-
+  
 }
--- sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
@@ -13,4 +13,13 @@ public class Point {
     this.y = y;
   }
 
+  public Point() {
+
+  }
+
+  public double distance(Point p1, Point p2) {
+    double x = p1.x - p2.x;
+    double y = p1.y - p2.y;
+    return Math.sqrt(x * x + y * y);
+  }
 }
