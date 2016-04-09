510e2247ec13ff2946610236a3407b6d7290ae16
aneznamova <an.neznamova@gmail.com>
2016-04-09 20:35:46 +0300

    Добавлено предусловие для теста проверяющего корректность адреса, телефонов и почты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -13,7 +14,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by neznaa on 4/9/2016.
  */
 public class ContactInfoTest extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (app.contact().list().size() == 0) app.contact().create(new ContactData()
+                .withFirstname("Semen").withLastname("Efimov").withAddress("Spb Test street 23 kv.76")
+                .withMobilephone("9213000000").withHomephone("34-323-55").withWorkphone("43 23 212")
+                .withEmail("name-nick@test.com").withEmail2("test@test.ru").withEmail3("test@test111.com").withGroup("test1"));
+    }
 
+    
     @Test
     public void testContactInfo() {
         app.goTo().goToHomePage();
