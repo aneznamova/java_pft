ba44b53bb5955dc869298c20cc0ad9f102b0b99f
aneznamova <an.neznamova@gmail.com>
2016-04-15 18:46:44 +0300

    Выполнено подключение к БД при помощи ORM

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,16 +3,31 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
 
 @XStreamAlias("group")
+@javax.persistence.Entity
+@javax.persistence.Table(name="group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "group_name")
   private String name;
   @Expose
+  @Column(name = "group_header")
+  @Type(type="text")
   private String header;
   @Expose
+  @Column(name = "group_footer")
+  @Type(type="text")
   private String footer;
 
   public int getId() {
