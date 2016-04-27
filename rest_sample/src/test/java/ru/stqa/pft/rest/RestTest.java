26122bd494670db8782cc1ebc1fa81cf3c02dcc8
aneznamova <an.neznamova@gmail.com>
2016-04-27 17:56:33 +0300

    Реализована интеграция тестов с баг-трекером Bugify. Перед выполнением теста создания issue проверяется загрыт ли связанный с ним баг.

--- rest_sample/src/test/java/ru/stqa/pft/rest/RestTest.java
+++ rest_sample/src/test/java/ru/stqa/pft/rest/RestTest.java
@@ -7,9 +7,12 @@ import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.Set;
 
 import static org.testng.AssertJUnit.assertEquals;
@@ -17,7 +20,13 @@ import static org.testng.AssertJUnit.assertEquals;
 /**
  * Created by neznaa on 4/26/2016.
  */
-public class RestTest {
+public class RestTest extends TestBase{
+
+    @BeforeMethod
+    public void checkIssueStatus() throws IOException {
+        int issueId = 1;
+        skipIfNotFixed(issueId);
+    }
 
     @Test
     public void testCreateIssue() throws IOException {
@@ -28,28 +37,4 @@ public class RestTest {
         oldIssues.add(newIssue.withId(issueId));
         assertEquals(newIssues, oldIssues);
     }
-
-    private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-                .returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-
-    }
-
-    private Executor getExecutor() {
-        return org.apache.http.client.fluent.Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
-    }
-
-    private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                          new BasicNameValuePair("description", newIssue.getDescription())))
-                .returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        return parsed.getAsJsonObject().get("issue_id").getAsInt();
-    }
-
-
 }
